324d84c02300797d7ba062fdb48c5954d9ed5cfe
ash-greninja <uzairshabbirsab@gmail.com>
2017-08-19 05:54:27 -0700

    CAS Project Created

---
+++ Create A Server.js
@@ -0,0 +1,21 @@
+/*********************
+***Create-A-Server****
+*********************/
+/** Connection Settings */
+export.port '' /** The Port of Connection. if you don't know about it, set it to 2000 because 2000
+port is default for connection. */
+export.invite '' /** Invitation link to server. if you want to create your own server, type invitation
+link of localhost-psim-ps. if you don't know where it is than, See the Pin massages of news-psim-ps or
+news-pokesite-ps. If you still can't find it, P.M Uzair#2801 to get one.  */
+export.psim '' /** Type the psim. for example:
+* psim-ps or psim-eu */
+export.title '' /** Title of your Server. For Example:
+* Safari Zone */
+export.url '' /** Finally, A intrested thing! type the domain/web address of your server. For Example:
+* If you will type wisp in export.url '' & type psim-ps in export.psim '' than, the server will
+be created as the name of wisp-psim-ps */
+export.admin '' /** Admins name(with code). for example:
+Uzair#2801
+* for multiple admins type:
+export.admin '[name1],[name2]'
+*/
